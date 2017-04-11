005dcd9c76177dbac13f9c3fd64be1327cffcb1e
anhhuycan83 <anhhuycan83@gmail.com>
2017-04-11 11:37:32 +0700

    add new project files
    First commit

---
+++ RunDosCommand/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace RunDosCommand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtGPSPFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGPSPFile = new System.Windows.Forms.Button();
+            this.dtpBeginning = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEnding = new System.Windows.Forms.Label();
+            this.dtpEnding = new System.Windows.Forms.DateTimePicker();
+            this.btnINIFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtINIFile = new System.Windows.Forms.TextBox();
+            this.btnLogFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLogFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(116, 156);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "&Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(197, 156);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtGPSPFile
+            // 
+            this.txtGPSPFile.Location = new System.Drawing.Point(70, 15);
+            this.txtGPSPFile.Name = "txtGPSPFile";
+            this.txtGPSPFile.Size = new System.Drawing.Size(149, 20);
+            this.txtGPSPFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GPSP file";
+            // 
+            // btnGPSPFile
+            // 
+            this.btnGPSPFile.Location = new System.Drawing.Point(225, 13);
+            this.btnGPSPFile.Name = "btnGPSPFile";
+            this.btnGPSPFile.Size = new System.Drawing.Size(34, 23);
+            this.btnGPSPFile.TabIndex = 4;
+            this.btnGPSPFile.Text = "...";
+            this.btnGPSPFile.UseVisualStyleBackColor = true;
+            this.btnGPSPFile.Click += new System.EventHandler(this.btnGPSPFile_Click);
+            // 
+            // dtpBeginning
+            // 
+            this.dtpBeginning.CustomFormat = "MM/dd/yyyy";
+            this.dtpBeginning.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBeginning.Location = new System.Drawing.Point(70, 100);
+            this.dtpBeginning.Name = "dtpBeginning";
+            this.dtpBeginning.Size = new System.Drawing.Size(121, 20);
+            this.dtpBeginning.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Begin";
+            // 
+            // lblEnding
+            // 
+            this.lblEnding.AutoSize = true;
+            this.lblEnding.Location = new System.Drawing.Point(35, 129);
+            this.lblEnding.Name = "lblEnding";
+            this.lblEnding.Size = new System.Drawing.Size(26, 13);
+            this.lblEnding.TabIndex = 7;
+            this.lblEnding.Text = "End";
+            // 
+            // dtpEnding
+            // 
+            this.dtpEnding.CustomFormat = "MM/dd/yyyy";
+            this.dtpEnding.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpEnding.Location = new System.Drawing.Point(70, 126);
+            this.dtpEnding.Name = "dtpEnding";
+            this.dtpEnding.Size = new System.Drawing.Size(121, 20);
+            this.dtpEnding.TabIndex = 8;
+            // 
+            // btnINIFile
+            // 
+            this.btnINIFile.Location = new System.Drawing.Point(225, 42);
+            this.btnINIFile.Name = "btnINIFile";
+            this.btnINIFile.Size = new System.Drawing.Size(34, 23);
+            this.btnINIFile.TabIndex = 11;
+            this.btnINIFile.Text = "...";
+            this.btnINIFile.UseVisualStyleBackColor = true;
+            this.btnINIFile.Click += new System.EventHandler(this.btnINIFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ini file";
+            // 
+            // txtINIFile
+            // 
+            this.txtINIFile.Location = new System.Drawing.Point(70, 44);
+            this.txtINIFile.Name = "txtINIFile";
+            this.txtINIFile.Size = new System.Drawing.Size(149, 20);
+            this.txtINIFile.TabIndex = 9;
+            // 
+            // btnLogFolder
+            // 
+            this.btnLogFolder.Location = new System.Drawing.Point(225, 71);
+            this.btnLogFolder.Name = "btnLogFolder";
+            this.btnLogFolder.Size = new System.Drawing.Size(34, 23);
+            this.btnLogFolder.TabIndex = 14;
+            this.btnLogFolder.Text = "...";
+            this.btnLogFolder.UseVisualStyleBackColor = true;
+            this.btnLogFolder.Click += new System.EventHandler(this.btnLogFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Log folder";
+            // 
+            // txtLogFolder
+            // 
+            this.txtLogFolder.Location = new System.Drawing.Point(70, 73);
+            this.txtLogFolder.Name = "txtLogFolder";
+            this.txtLogFolder.Size = new System.Drawing.Size(149, 20);
+            this.txtLogFolder.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLogFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLogFolder);
+            this.Controls.Add(this.btnINIFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtINIFile);
+            this.Controls.Add(this.dtpEnding);
+            this.Controls.Add(this.lblEnding);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpBeginning);
+            this.Controls.Add(this.btnGPSPFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGPSPFile);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtGPSPFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGPSPFile;
+        private System.Windows.Forms.DateTimePicker dtpBeginning;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEnding;
+        private System.Windows.Forms.DateTimePicker dtpEnding;
+        private System.Windows.Forms.Button btnINIFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtINIFile;
+        private System.Windows.Forms.Button btnLogFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLogFolder;
+    }
+}
+
---
+++ RunDosCommand/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace RunDosCommand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtGPSPFile.Text))
+            {
+                MessageBox.Show("GPSP file does not exist");
+                return;
+            }
+                
+            if (!File.Exists(txtINIFile.Text))
+            {
+                MessageBox.Show("ini file does not exist");
+                return;
+            }
+            if (!Directory.Exists(txtLogFolder.Text))
+            {
+                MessageBox.Show("Log folder does not exist");
+                return;
+            }
+            Process myProcess = new Process();
+            TimeSpan tsNum = dtpEnding.Value - dtpBeginning.Value;
+            int numDaily = (int) tsNum.TotalDays + 1;
+
+            myProcess.StartInfo.FileName = txtGPSPFile.Text;
+
+            //Do not receive an event when the process exits.
+            myProcess.EnableRaisingEvents = false;
+
+            //myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.StartInfo.CreateNoWindow = false;
+            myProcess.StartInfo.UseShellExecute = true;
+            //myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+            DateTime dtDaily = dtpBeginning.Value;
+
+            for (int i = 0; i < numDaily; i++)
+            {
+                try
+                {
+                    
+                    // Parameters
+                    //myProcess.StartInfo.Arguments = @"C:\ELT\Live\Server\Edulognt.cfg 1 0 1 1 1 1";
+                    
+
+                    myProcess.StartInfo.Arguments = txtINIFile.Text + " " + dtDaily.ToString("MM/dd/yyyy");
+                    
+
+                    myProcess.Start();
+
+                    int milisecondToWait = 600000; // 10 minute
+                    myProcess.WaitForExit(milisecondToWait);
+
+                    //myProcess.WaitForExit(milisecondToWait);
+
+                    ////check the fStopGeo.txt file
+                    ////To get the location the assembly normally resides on disk or the install directory
+                    //string path = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+
+                    ////once you have the path you get the directory with:
+                    //string directory = System.IO.Path.GetDirectoryName(path);
+
+                    string resultFile = txtLogFolder.Text + "\\" + dtDaily.ToString("MMddyyyy") + "-postprocess.log";
+                    //int milisecondToWait = 600000;
+                    int count = 0;
+                    bool founded = false;
+                    while (count < 30)
+                    {
+                        count++;
+                        if (File.Exists(resultFile))
+                        {
+                            ////get this stop infor node1 node2, d1, d2
+                            //StringBuilder sb = new StringBuilder();
+                            ////bool containt = false;
+
+                            //StreamReader reader = new StreamReader("");
+                            //string line = reader.ReadLine();
+                            ////bool containt = false;
+                            ////string[] stopInfor;
+                            break;
+                        }
+                        else
+                            Thread.Sleep(1000);
+                    }
+
+                    dtDaily = dtDaily.AddDays(1);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex.Message);
+                }
+
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGPSPFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "*|*.exe";
+            openFileDialog1.Title = "Select a PostProcess.exe file";
+
+            string filePath;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                txtGPSPFile.Text = filePath;
+            }
+        }
+
+        private void btnINIFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Filter = "*|*.ini";
+            openFileDialog2.Title = "Select a gpsp.ini file";            
+
+            string filePath;
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = openFileDialog2.FileName;
+                txtINIFile.Text = filePath;
+            }
+        }
+
+        private void btnLogFolder_Click(object sender, EventArgs e)
+        {
+            //String folder;
+            //FolderBrowserDialog fbd = new FolderBrowserDialog();
+            ////fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            //if (fbd.ShowDialog() == DialogResult.OK)
+            //{
+            //    folder = fbd.SelectedPath;
+            //    txtLogFolder.Text = fbd.SelectedPath;
+            //}
+
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Filter = "*|*.*";
+            openFileDialog2.Title = "Select a any file in log folder";
+
+            string filePath;
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = Path.GetDirectoryName(openFileDialog2.FileName);
+                txtLogFolder.Text = filePath;
+            }
+        }
+    }
+}
